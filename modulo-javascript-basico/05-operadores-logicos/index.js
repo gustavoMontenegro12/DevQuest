e9bcb48247fd34eb0e82cbd84c266985237c6454
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-09-09 19:57:59 -0300

    Adicionada a aula sobre operadores logicos

---
+++ modulo-javascript-basico/05-operadores-logicos/index.js
@@ -0,0 +1,15 @@
+const a = 15
+const b = 20
+
+function imprime(item){
+    console.log(item)
+}
+
+imprime(a==b)
+imprime(a===b)
+imprime(a!=b)
+imprime(a!==b)
+imprime(a < b)
+imprime(a <= b)
+imprime(a > b)
+imprime(a >= b)
