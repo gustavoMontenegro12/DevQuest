38cccccca9f0dd0d20dd4fc2bdf239c8efee8312
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-09-08 21:00:14 -0300

    Adicionada um pouco de logica e algimas variaveis no código junto de dois ifs

--- modulo-javascript-basico/01-meu-primeiro-codigo-js/index.js
+++ modulo-javascript-basico/01-meu-primeiro-codigo-js/index.js
@@ -1,2 +1,14 @@
-let idade = window.prompt("Digite sua idade")
+let confirmacao = window.confirm("Deseja continuar?")
+console.log(confirmacao)
 
+if(confirmacao == true){
+
+    let idade = window.prompt("Digite sua idade")
+    console.log(typeof idade)
+
+    if (idade >= 40 ){
+        window.alert("Tá velho(a), hein??")
+    }else{
+        window.alert("tá na faixa ainda")
+    }
+}
