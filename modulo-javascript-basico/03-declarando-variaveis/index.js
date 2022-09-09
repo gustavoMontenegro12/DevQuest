11f02e072e9f6c236d21dd957fcf15d07d8099d9
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-09-08 21:09:49 -0300

    Adicionada a ula sobre declaracao de variaveis e seus escopos

---
+++ modulo-javascript-basico/03-declarando-variaveis/index.js
@@ -0,0 +1,11 @@
+{
+    var nomePessoa = "Gustavo"
+    const cpf = 4342258223366
+    let email = "gustavo@gmail.com"
+}
+
+console.log(nomePessoa)
+console.log(cpf)
+console.log(email)
+
+// cpf e email estão fora do escopo, ou seja utilizavel apenas dentro do bloco em foram declaradas fazendo com que o console.log não as enxergue resultando no erro de compilação.
