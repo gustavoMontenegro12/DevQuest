02f817d878844258cf9c2347a9d48b9fa4b0dcd0
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-09-13 19:32:48 -0300

    Adciionado a aula sobre operadores matematicos

---
+++ modulo-javascript-basico/06-operadores-matematicos/index.js
@@ -0,0 +1,17 @@
+const primeiroValor = 25
+const segundoValor = 11
+
+//Soma
+console.log(primeiroValor + segundoValor)
+
+//Subtração
+console.log(primeiroValor - segundoValor)
+
+//Multiplicação
+console.log(primeiroValor * segundoValor)
+
+//Divisão
+console.log(primeiroValor / segundoValor)
+
+//Resto
+console.log(primeiroValor % segundoValor)
