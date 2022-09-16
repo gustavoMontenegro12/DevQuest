6a22d681cc15dcb63bbc367b104d64c3c1f21033
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-09-16 20:02:59 -0300

    Adicionada a aula sobre função no js

---
+++ modulo-javascript-basico/11-funcao/index.js
@@ -0,0 +1,12 @@
+function imprime(texto = "Seu texto aparecerá aqui"){
+    console.log(texto)
+}
+
+imprime("Olá mundão")
+imprime()
+
+function soma(numero1 = 0, numero2 = 0){
+    imprime(numero1 + numero2)
+}
+
+soma(2,5)
