b041200c61db8e86e6078b1f0c43420a78072dd0
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-09-26 21:03:44 -0300

    Adicionado a aula de js intermediario sobre funcoes anonimas

---
+++ modulo-javascript-intermediario/01-funcoes-anonimas/index.js
@@ -0,0 +1,16 @@
+// function somar(numero1 = 0, numero2 = 0){
+//     return numero1 + numero2
+// }
+
+let resultadoSoma = function(numero1 = 0, numero2 = 0){
+    return numero1 + numero2
+}
+
+console.log(`O resultado da soma é ${resultadoSoma(5, 10)}`)
+
+let resultadoDivisao = function(numero1 = 1, numero2 = 1){
+    return numero1 / numero2
+}
+
+console.log(`O resultado da divisão é ${resultadoDivisao(852,23)}`)
+
