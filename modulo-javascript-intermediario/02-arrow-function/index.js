900143e4e52cca14921fa693c775d8fbc399aabf
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-09-28 18:34:06 -0300

    Adicionado a aula sobre as arrow functions

---
+++ modulo-javascript-intermediario/02-arrow-function/index.js
@@ -0,0 +1,7 @@
+// let somar = function(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+let somar = (numero1, numero2) =>  numero1 + numero2
+
+console.log(`O resultado : ${somar(10, 10)}`)
