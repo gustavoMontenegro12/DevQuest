6af0bb7ec7754d90201ba5f9d08750797d33db7c
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-09-30 17:47:15 -0300

    Adicionada a aula inicial sobre arrays

---
+++ modulo-javascript-intermediario/04-arrays/index.js
@@ -0,0 +1,5 @@
+let filmes = [["Blade Runner", 10.0, true], ["Blade runner 2049,10.0, true"], ["Duna", 10.0, true]]
+
+let mangas = [["Blade, a lâmina do imortal", "realista"], ["Berserk", "realista"]]
+
+console.table(`Mangá: Título ${mangas[0][0]} Estilo de arte ${mangas[0][1]}`)
