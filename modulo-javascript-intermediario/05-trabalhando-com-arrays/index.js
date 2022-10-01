0070687a439ef9436975c2238c61942780a421e6
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-10-01 18:54:04 -0300

    Adicionado os metodos splice e slice

--- modulo-javascript-intermediario/05-trabalhando-com-arrays/index.js
+++ modulo-javascript-intermediario/05-trabalhando-com-arrays/index.js
@@ -4,8 +4,13 @@ frutas.shift()
 frutas.pop()
 frutas.push("Morango")
 frutas.unshift("Uva")
+let frutasSlice = frutas.slice(0, 2)
+frutas.splice(3, 0, "Goiaba", "Banana", "Maçã")
+
 
 frutas.forEach((item, index) => {
     console.log(`Item - Frutas: ${item}`)
     console.log(`Index - Frutas: ${frutas[index]}`)
 })
+
+console.log(frutasSlice)
