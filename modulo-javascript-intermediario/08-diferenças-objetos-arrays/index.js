e2025c2f85c600dd0c92c53b1b73235676d22b0f
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-10-03 19:35:01 -0300

    Adicionada a aula sobre diferenças entre objetos e arrays

---
+++ modulo-javascript-intermediario/08-diferenças-objetos-arrays/index.js
@@ -0,0 +1,15 @@
+let frutas = ["Maçã", "Goiaba", "Maracujá"]
+
+let videogame1 = {
+    nome: "Xbox",
+    valor: 3000
+}
+
+let videogame2 = {
+    nome: "Playstation",
+    valor: 5000
+}
+
+let videogames = [videogame1, videogame2]
+
+console.log(videogames[0].nome)
